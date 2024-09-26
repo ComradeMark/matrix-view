7f32a4e05a5358ec245b95a7dbedf15f2bc29a64
Ahmet <hacksmithinacoderverse@gmail.com>
2024-09-26 10:30:42 -0400

    Fixed minor bug related to mainClass ref

--- src/main/java/edu/vanier/matrixView/math/Matrix.java
+++ src/main/java/edu/vanier/matrixView/math/Matrix.java
@@ -1,4 +1,26 @@
 package edu.vanier.matrixView.math;
 
 public class Matrix {
+    public double x;
+    public double y;
+
+    public Matrix(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+
+    @Override
+    public String toString() {
+        return ("(" + x + "," + y + ")");
+    }
 }
