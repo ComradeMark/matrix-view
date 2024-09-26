51ccce9e3775d40a58a546de0929c99b4fbc5ce8
Ahmet <hacksmithinacoderverse@gmail.com>
2024-09-26 11:12:44 -0400

    Created the initial non-finished version of the calculator class

--- src/main/java/edu/vanier/matrixView/math/Calculator.java
+++ src/main/java/edu/vanier/matrixView/math/Calculator.java
@@ -1,4 +1,20 @@
 package edu.vanier.matrixView.math;
 
-public class Calculator {
+import jdk.jshell.spi.ExecutionControl;
+
+public class Calculator  {
+
+
+//    public static Vector Multiply(Matrix matrix, Coordinate coordinate) throws ExecutionControl.NotImplementedException {
+//
+//        throw new ExecutionControl.NotImplementedException("Not implemented");
+//    }
+
+    public static Vector[] matrixMultiply(Matrix matrix, Coordinate[] coordinate) throws ExecutionControl.NotImplementedException {
+
+        throw new ExecutionControl.NotImplementedException("Not implemented");
+    }
+
+    
+
 }
