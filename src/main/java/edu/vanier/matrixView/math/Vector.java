f78c4dd1464192964a0eb530154950cb02840822
Ahmet <hacksmithinacoderverse@gmail.com>
2024-09-26 11:00:57 -0400

    Created the intial version of the vector class

--- src/main/java/edu/vanier/matrixView/math/Vector.java
+++ src/main/java/edu/vanier/matrixView/math/Vector.java
@@ -1,4 +1,20 @@
 package edu.vanier.matrixView.math;
 
-public class Vector {
+/**
+ * Created the initial version of the Vector class (inherits Coordinate)
+ *
+ * Magnitude is being calculated from the origin (by definition of a vector)
+ */
+
+
+public class Vector extends Coordinate{
+    double magnitude;
+    public Vector(double x, double y) {
+        super(x, y);
+        magnitude = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) );
+    }
+
+    public double getMagnitude() {
+        return magnitude;
+    }
 }
--- src/main/java/edu/vanier/matrixView/test/Driver.java
+++ src/main/java/edu/vanier/matrixView/test/Driver.java
@@ -1,4 +1,7 @@
 package edu.vanier.matrixView.test;
 
 public class Driver {
+    public static void main(String[] args) {
+
+    }
 }
