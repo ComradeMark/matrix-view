32c5b299dadd5928891af60fbd2fc073356d1316
Mark <jm.partners00@gmail.com>
2024-10-21 09:14:37 -0400

    Added fxml annotations

--- src/main/java/edu/vanier/matrixView/controllers/MainAppFXMLController.java
+++ src/main/java/edu/vanier/matrixView/controllers/MainAppFXMLController.java
@@ -4,37 +4,73 @@ import edu.vanier.matrixView.MainApp;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * FXML controller class for the primary stage's scene.
+ * FXML controller for controlling the main window.
  *
- * @author frostybee
  */
 public class MainAppFXMLController {
 
     private final static Logger logger = LoggerFactory.getLogger(MainAppFXMLController.class);
 
     @FXML
-    Button btnClickMe;
+    TitledPane configPane;
+    @FXML
+    Spinner spinnerA;
+    @FXML
+    Spinner spinnerB;
+    @FXML
+    Spinner spinnerC;
+    @FXML
+    Spinner spinnerD;
+
+    @FXML
+    TitledPane valuePane;
+    @FXML
+    TextField fieldDet;
+    @FXML
+    TextField transA;
+    @FXML
+    TextField transB;
+    @FXML
+    TextField transC;
+    @FXML
+    TextField transD;
+    @FXML
+    TextField invA;
+    @FXML
+    TextField invB;
+    @FXML
+    TextField invC;
+    @FXML
+    TextField invD;
+
+    @FXML
+    TitledPane controlPane;
+    @FXML
+    Button btnGenerate;
+    @FXML
+    Button btnReset;
+
     @FXML
     Button btnSwitchScene;
 
     @FXML
     public void initialize() {
         logger.info("Initializing MainAppController...");
-        btnClickMe.setOnAction(this::handleClickMe);
-        btnSwitchScene.setOnAction(this::loadSecondaryScene);
+
     }
 
     private void handleClickMe(Event e) {
-        System.out.println("Click me called.");
-        logger.info("Click me button has been pressed...");
+
     }
 
     private void loadSecondaryScene(Event e) {
-        MainApp.switchScene(MainApp.SECONDARY_LAYOUT, new SecondaryFXMLController());
-        logger.info("Loaded the secondary scene...");
+
     }
 }
